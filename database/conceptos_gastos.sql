c01b91993b1af0181b9cb661ba7e1ddc338fb94d
Jakku Night <J97k107u45k117n@gmail.com>
2023-08-09 15:26:40 -0400

    Splited conceptos.sql into two files.

---
+++ database/conceptos_gastos.sql
@@ -0,0 +1,29 @@
+-- Datos de prueba:
+INSERT INTO  concepto_gastos ( concepto, descripcion ) VALUES(
+	"ALIMENTACIÓN",
+	"Es el dinero que se destina a la compra de alimentos."
+);
+INSERT INTO  concepto_gastos ( concepto, descripcion ) VALUES(
+	"AHORRO",
+	"Es dinero que se decide guardar para situaciones de necesidad o inversiones."
+);
+INSERT INTO  concepto_gastos ( concepto, descripcion ) VALUES(
+	"TRANSPORTE",
+	"Es el dinero destinado a transportarse o algún medio."
+);
+INSERT INTO  concepto_gastos ( concepto, descripcion ) VALUES(
+	"IMPUESTOS",
+	"Es el dinero que se paga al Estado como tributo."
+);
+INSERT INTO  concepto_gastos ( concepto, descripcion ) VALUES(
+	"SERVICIOS BÁSICOS",
+	"Es el dinero destinado a pagar servicios como el agua corriente, la electricidad, etc."
+);
+INSERT INTO  concepto_gastos ( concepto, descripcion ) VALUES(
+	"VIVIENDA",
+	"Es el dinero destinado a los costes de la vivienda."
+);
+INSERT INTO  concepto_gastos ( concepto, descripcion ) VALUES(
+	"SALUD",
+	"Es el dinero utilizado para afrontar enfermedades o mantener una buena salud."
+);
