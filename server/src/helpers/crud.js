4892649ce9761b7430d41d63690887213da834a6
Jakku Night <J97k107u45k117n@gmail.com>
2023-08-11 16:31:11 -0400

    Added basic CRUD functions.

--- server/src/helpers/crud.js
+++ server/src/helpers/crud.js
@@ -1,50 +1,63 @@
 const pool = require('../db.js');
 const chalk = require('chalk');
 
-const leerTabla = async (tabla) => {
-	try{
+// Funciones CRUD:
+async function leerTabla(tabla) {
+	try {
 		const datos = (await pool.query("SELECT * FROM " + tabla))[0];
 		console.log(chalk.blueBright("[INFO] Se leyeron los datos de '" + tabla + "'"));
 		return datos;
-	}catch(e){
+	} catch (e) {
 		console.error(chalk.redBright(JSON.stringify(e)));
 		return;
 	}
-};
+}
 
-const insertarFila = async (tabla, fila) => {
-	try{
-		const resultado = (await pool.query("INSERT INTO " + tabla + " SET ?", [ fila ]))[0];
+async function insertarFila(tabla, fila) {
+	try {
+		const resultado = (await pool.query("INSERT INTO " + tabla + " SET ?", [fila]))[0];
 		console.log(resultado);
 		console.log(chalk.greenBright("[INFO] Se insertaron datos en '" + tabla + "'"));
 		return resultado;
-	}catch(e){
+	} catch (e) {
 		console.error(chalk.redBright(JSON.stringify(e)));
 		return;
 	}
-};
+}
 
-const actualizarFila = async (tabla, nuevaFila, id) => {
-	try{
-		const result = (await pool.query("UPDATE " + tabla + " SET ? WHERE id = ?", [ nuevaFila, id ]))[0];
+async function actualizarFila(tabla, nuevaFila, id) {
+	try {
+		const result = (await pool.query("UPDATE " + tabla + " SET ? WHERE id = ?", [nuevaFila, id]))[0];
 		console.log(chalk.blueBright("[INFO] Se actualizaron los datos de '" + tabla + "' en la fila '" + id + "'"));
 		return result;
-	}catch(e){
+	} catch (e) {
 		console.error(chalk.redBright(JSON.stringify(e)));
 		return;
 	}
-};
+}
 
-const borrarFila = async (tabla, id) => {
-	try{
-		const result = (await pool.query("DELETE FROM " + tabla + " WHERE id = ?", [ id ]))[0];
+async function borrarFila(tabla, id) {
+	try {
+		const result = (await pool.query("DELETE FROM " + tabla + " WHERE id = ?", [id]))[0];
 		console.log(chalk.blueBright("[INFO] Se eliminaron datos de '" + tabla + "' en la fila '" + id + "'"));
 		return result;
-	}catch(e){
+	} catch (e) {
 		console.error(chalk.redBright(JSON.stringify(e)));
 		return;
 	}
-};
+}
+
+// Funciones adicionales:
+async function encontrarFila({ tabla, campo = "id", valor = 1 } = {}) {
+	try {
+		return true;
+	} catch (e) {
+		console.error(chalk.redBright(JSON.stringify(e)));
+		return false;
+	}
+}
+
+encontrarFila()
 
 module.exports = {
 	leerTabla,
